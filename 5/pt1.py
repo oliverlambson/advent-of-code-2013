ff70e6402532bd177b87111e96696a6f3af98b1f
Oliver Lambson <oliverlambson@gmail.com>
2023-12-08 20:59:18 +0000

    5.1 done

--- 5/pt1.py
+++ 5/pt1.py
@@ -1,4 +1,3 @@
-from tqdm import tqdm
 import logging
 from typing import Callable
 from pathlib import Path
@@ -18,24 +17,28 @@ class Map:
     mapping: Callable[[int], int]
 
 
+def mapping_fn_factory(base_range, target_range) -> Callable[[int], int | None]:
+    def inner(x: int) -> int | None:
+        logging.debug(f"{base_range}:{target_range} {x}")
+        if x not in base_range:
+            return None
+        idx = x - base_range.start
+        return target_range[idx]
+    return inner
+
+
 def parse_map(map_raw: str) -> Map:
     m = map_raw.strip().split("\n")
     base, target = map(lambda s: s.strip(), m[0].rstrip(" map:").split("-to-"))
     mappings_raw = map(lambda s: s.strip(), m[1:])
 
-    mapping_fns = []
+    mapping_fns: list[Callable[[int], int | None]] = []
     for mapping_raw in (mappings_raw):
         range_strings = mapping_raw.split()
         target_start, base_start, length = list(map(int, range_strings))
         base_range = range(base_start, base_start+length)
         target_range = range(target_start, target_start+length)
-        # mapping = {base: target for base, target in zip(base_range, target_range)}
-        def mapping_fn(x: int) -> int | None:
-            logging.debug(f"{base_range}:{target_range} {x}")
-            if x not in base_range:
-                return None
-            idx = x - base_range.start
-            return target_range[idx]
+        mapping_fn = mapping_fn_factory(base_range, target_range)
         mapping_fns.append(mapping_fn)
 
     def master_mapping_fn(x: int) -> int:
@@ -58,6 +61,7 @@ def parse_map(map_raw: str) -> Map:
         mapping=master_mapping_fn,
     )
 
+
 def derive_map(base: str, target: str, mappings: dict[str, Map]) -> Callable[[int], int]:
     current_map = mappings.get(base)
     if current_map is None:
@@ -67,37 +71,37 @@ def derive_map(base: str, target: str, mappings: dict[str, Map]) -> Callable[[in
         return map_fn
     return lambda x: derive_map(current_map.target, target, mappings)(map_fn(x))
 
-# def main():
-input_file_name = "input.txt"
-TEST = True
-if TEST:
-    logging.basicConfig(level=logging.DEBUG)
-    input_file_name = "example_input.txt"
-input_file = Path(__file__).parent / input_file_name
-input_file_txt = input_file.read_text().strip()
 
-input_blocks = input_file_txt.split("\n\n")
+def main():
+    input_file_name = "input.txt"
+    TEST = False
+    if TEST:
+        logging.basicConfig(level=logging.DEBUG)
+        input_file_name = "example_input.txt"
+    input_file = Path(__file__).parent / input_file_name
+    input_file_txt = input_file.read_text().strip()
 
-targets_raw = input_blocks[0]
-maps_raw = input_blocks[1:]
+    input_blocks = input_file_txt.split("\n\n")
 
-targets = Targets(
-    name=targets_raw.split(": ")[0],
-    numbers=list(map(int, targets_raw.split(": ")[1].split()))
-)
+    targets_raw = input_blocks[0]
+    maps_raw = input_blocks[1:]
 
-mappings = {}
-for map_raw in maps_raw:
-    logging.debug(map_raw)
-    m = parse_map(map_raw)
-    mappings[m.base] = m
-    logging.debug("")
+    targets = Targets(
+        name=targets_raw.split(": ")[0],
+        numbers=list(map(int, targets_raw.split(": ")[1].split()))
+    )
 
-seed_to_location = derive_map("seed", "location", mappings)
-locations = map(seed_to_location, targets.numbers)
-print(min(locations))
+    mappings = {}
+    for map_raw in maps_raw:
+        logging.debug(map_raw)
+        m = parse_map(map_raw)
+        mappings[m.base] = m
+        logging.debug("")
+
+    seed_to_location = derive_map("seed", "location", mappings)
+    locations = map(seed_to_location, targets.numbers)
+    print(min(locations))
 
 
-
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
